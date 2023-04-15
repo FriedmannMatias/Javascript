5848e0be8bb2e501ae0e8efd5b20be7b2e257ee0
Matias Friedmann <friedmann.matias@gmail.com>
2023-04-14 21:54:09 -0300

    subida de los primeros archivos

---
+++ main.js
@@ -0,0 +1,32 @@
+
+function precioConIva (precioProducto){
+    let resultado = (precioProducto * 1.21)
+    return resultado;
+}
+
+function precioConDescuento (precioProducto){
+    let resultado = (precioConIva(precioProducto) - (precioProducto / 100 * 20))
+    return resultado;
+}
+
+while (true){
+    var precioProducto = prompt("ingresa el precio del producto que desea comprar"); //con var si anda, con let no
+    if(!isNaN(precioProducto) && precioProducto != null && precioProducto != " "){
+    break;
+    }else {
+        alert("no ingreso un numero");
+    continue;
+    }
+}
+
+let promo = prompt("perfecto, su producto tiene un descuento del 20%, desea aplicarlo? escriba 'si' o 'no'")
+    switch (promo.toLowerCase()){
+        case "si":
+            alert("Muy bien, el precio del producto con el descuento aplicado y sumado el iva es de: " + precioConDescuento(precioProducto));
+            break;
+        case "no":
+            alert("Muy bien, el precio del producto sumado el iva es de: " + precioConIva(precioProducto));
+            break;
+        default:
+            alert("no ingreso ni 'si', ni 'no'")
+        }
